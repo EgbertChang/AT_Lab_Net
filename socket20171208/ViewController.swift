e96e799a0cd19af05cf8edaa418e0b89df02e4f8
egbertchang <egbertchang@icloud.com>
2017-12-08 21:27:37 +0800

    first commit

---
+++ socket20171208/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  socket20171208
+//
+//  Created by Egbert Chang on 08/12/2017.
+//  Copyright © 2017 Aleph Tdu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
