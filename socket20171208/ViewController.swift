c841bdd6713198eaaa5d620ca1f3f51da6750c8f
egbertchang <egbertchang@icloud.com>
2017-12-11 10:20:59 +0800

    feat: t

--- socket20171208/ViewController.swift
+++ socket20171208/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.red
+        self.view.backgroundColor = UIColor.blue
         // Do any additional setup after loading the view, typically from a nib.
     }
 
